abb214f758e754ca6ab8ab1b913fece925aae629
Jon Young <jey6639@gmail.com>
2023-06-22 20:20:07 -0500

    created constructor and tables

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+'use strict';
 const hoursOpen = [
+  '',
   '6am',
   '7am',
   '8am',
@@ -16,86 +18,80 @@ const hoursOpen = [
   '8pm',
 ];
 
-const seattle = {
-  name: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  aveCookieSales: 6.3,
-  randomNumberOfCustomers: function () {
-    return Math.floor(
-      Math.random() * (this.maxCust - this.minCust) + this.maxCust
-    );
-  },
+function City(name, minCust, maxCust, aveCookieSales) {
+  this.name = name;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.aveCookieSales = aveCookieSales;
+}
+City.prototype.randomNumberOfCustomers = function () {
+  return Math.floor(
+    Math.random() * (this.maxCust - this.minCust) + this.maxCust
+  );
 };
-const tokyo = {
-  name: 'Tokyo',
-  minCust: 3,
-  maxCust: 24,
-  aveCookieSales: 1.2,
-  randomNumberOfCustomers: function () {
-    return Math.floor(
-      Math.random() * (this.maxCust - this.minCust) + this.maxCust
-    );
-  },
-};
-const dubai = {
-  name: 'Dubai',
-  minCust: 11,
-  maxCust: 38,
-  aveCookieSales: 3.7,
-  randomNumberOfCustomers: function () {
-    return Math.floor(
-      Math.random() * (this.maxCust - this.minCust) + this.maxCust
-    );
-  },
-};
-const paris = {
-  name: 'Paris',
-  minCust: 20,
-  maxCust: 38,
-  aveCookieSales: 2.3,
-  randomNumberOfCustomers: function () {
-    return Math.floor(
-      Math.random() * (this.maxCust - this.minCust) + this.maxCust
-    );
-  },
-};
-const lima = {
-  name: 'Lima',
-  minCust: 2,
-  maxCust: 16,
-  aveCookieSales: 4.6,
-  randomNumberOfCustomers: function () {
-    return Math.floor(
-      Math.random() * (this.maxCust - this.minCust) + this.maxCust
-    );
-  },
-};
-function cookieCount(city) {
-  let h2 = document.createElement('h2');
-  h2.innerText = city.name;
+City.prototype.render = function () {
   let main = document.getElementById('cities');
-  main.appendChild(h2);
-  let ul = document.createElement('ul');
-  main.appendChild(ul);
+  let table = document.createElement('table');
+  main.appendChild(table);
+  let headerRow = document.createElement('tr');
+  table.appendChild(headerRow);
+  for (let i = 0; i < hoursOpen.length; i++) {
+    let header = document.createElement('th');
+    header.textContent = hoursOpen[i];
+    headerRow.appendChild(header);
+  }
+
   let totalSales = 0;
   for (let i = 0; i < hoursOpen.length; i++) {
-    let li = document.createElement('li');
+    let cityDataRow = document.createElement('tr');
     let hourlySales = Math.floor(
-      city.randomNumberOfCustomers() * city.aveCookieSales
+      this.randomNumberOfCustomers() * this.aveCookieSales
     );
     totalSales += hourlySales;
     console.log(totalSales);
     li.innerText = `${hoursOpen[i]}: ${hourlySales} cookies`;
-    ul.appendChild(li);
   }
   let total = document.createElement('li');
   total.innerText = `Total: ${totalSales} cookies`;
   ul.appendChild(total);
-}
+};
+let seattle = new City('Seattle', 23, 65, 6.3);
+let tokyo = new City('Tokyo', 3, 24, 1.2);
+let dubai = new City('Dubai', 11, 38, 3.7);
+let paris = new City('Paris', 20, 38, 2.3);
+let lima = new City('Lima', 2, 16, 4.6);
+console.log(City);
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
 
-cookieCount(seattle);
-cookieCount(tokyo);
-cookieCount(dubai);
-cookieCount(paris);
-cookieCount(lima);
+// function cookieCount(city) {
+//   let h2 = document.createElement('h2');
+//   h2.innerText = city.name;
+//   let main = document.getElementById('cities');
+//   main.appendChild(h2);
+//   let ul = document.createElement('ul');
+//   main.appendChild(ul);
+//   let totalSales = 0;
+//   for (let i = 0; i < hoursOpen.length; i++) {
+//     let li = document.createElement('li');
+//     let hourlySales = Math.floor(
+//       city.randomNumberOfCustomers() * city.aveCookieSales
+//     );
+//     totalSales += hourlySales;
+//     console.log(totalSales);
+//     li.innerText = `${hoursOpen[i]}: ${hourlySales} cookies`;
+//     ul.appendChild(li);
+//   }
+//   let total = document.createElement('li');
+//   total.innerText = `Total: ${totalSales} cookies`;
+//   ul.appendChild(total);
+// }
+
+// cookieCount(seattle);
+// cookieCount(tokyo);
+// cookieCount(dubai);
+// cookieCount(paris);
+// cookieCount(lima);
